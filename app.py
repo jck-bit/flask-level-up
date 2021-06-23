fddd4c2f1ed67cceb14d34b97aa07503a018c57c
jck-bi <jckkinyanjui@gmail.com>
2021-06-23 16:15:37 +0300

    working with templates

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
+#whenever a function is called in front of a route,..whatever is returned is just spit to the browser
+
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 @app.route('/home/<string:name>')
 def hello(name):
@@ -14,3 +19,5 @@ def get_req():
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+##templtes are the front end of flask
