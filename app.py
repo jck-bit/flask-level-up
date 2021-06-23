bc81cfde0d73105ea5e7483e46d288870878950d
jck-bi <jckkinyanjui@gmail.com>
2021-06-23 16:07:29 +0300

    working out with some routes

--- app.py
+++ app.py
@@ -2,7 +2,15 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return 'hello world'
+@app.route('/home/<string:name>')
+def hello(name):
+    return 'hello, ' + name
 
+#i only allow a get request to the webpage below
+
+@app.route('/only get', methods=['GET'])
+def get_req():
+    return 'you can only get this webpage'
+
+if __name__ == "__main__":
+    app.run(debug=True)
