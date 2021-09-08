39b7dd2a8bd5cac6a940876ce399315c28b68cd3
jck-bi <jckkinyanjui@gmail.com>
2021-09-08 20:57:52 +0300

    adding some bootstrap

--- app.py
+++ app.py
@@ -1,16 +1,31 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 #whenever a function is called  after a route,..whatever is returned is just spit to the browser
 
+posts = [
+  {
+    'author': 'corey shafer',
+    'title' : 'Blog Post 1',
+    'content':'first post content',
+    'date_created':'september 21,2019'
+  },
+  {
+     'author': 'corey shafer',
+     'title' : 'Blog post 2',
+     'content':'second post content',
+     'date_created':'september 22,2019'
+  }
+]
+
 @app.route('/')
 def index():
-    return "<h1>home page</h1>"
+    return render_template('home.html', posts=posts)
 
 @app.route('/about')
 def about():
-    return "my about page"
+    return render_template('about.html', title=about)
 
 
 if __name__ == "__main__":
